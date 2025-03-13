28a838374f14acab89853d16bfcdc53627427535
LauraLuzhen <ll.rodriguez@iesnervion.es>
2025-03-13 14:34:04 +0100

    exm

--- Tema4/src/exm/Empleado.java
+++ Tema4/src/exm/Empleado.java
@@ -14,7 +14,7 @@ public class Empleado {
 		}
 	}
 	
-	public Empleado(String dni, String nombre, double sueldoBase, int horasExtras, double importeHorasExtras) {
+	public Empleado(String dni, String nombre, double sueldoBase, int horasExtras) {
 		
 	}
 	
--- Tema4/src/exm/EmpleadoMain.java
+++ Tema4/src/exm/EmpleadoMain.java
@@ -1,5 +1,32 @@
 package exm;
 
+import java.util.Scanner;
+
 public class EmpleadoMain {
 
+	public static void main(String[] args) {
+		int opc;
+		
+		Scanner reader = new Scanner(System.in);
+		
+		System.out.println("Bienvenido empleado!!");
+		
+		do {
+			menu();
+			opc = reader.nextInt();
+			reader.nextLine();
+			
+			
+		} while (opc != 0);
+	}
+	
+	public static void menu() {
+		System.err.println("Elige una opción: ");
+		System.out.println("1. Añadir empleado");
+		System.out.println("2. Listar empleados");
+		System.out.println("3. Modificar horas extras");
+		System.out.println("4. Modificar importe horas extras");
+		System.out.println("5. Eliminar empleado");
+		System.out.println("0. Salir");
+	}
 }
